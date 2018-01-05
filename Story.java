d0eb0d018644341045ab8df7a127bca7c075f5aa
ichasen <ichasen@stuy.edu>
2018-01-05 15:21:12 -0500

    added basic get, set methods

---
+++ Story.java
@@ -0,0 +1,13 @@
+public class Story{
+    String protagonist;
+    String fileSave;
+    public Story(){}
+    public String getProtagonist(){
+	return protagonist;
+    }
+    public void saveStory(String fileName){}
+    public String getFileSave(){
+	return fileSave;
+    }
+    public void main(){}
+}
