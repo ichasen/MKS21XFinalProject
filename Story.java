e07e368fd8e1f7410d94ce288c88c20a5fbfac4b
bchen70 <bchen17@stuy.edu>
2018-01-06 17:38:01 -0500

    Fixed Story so that it would compile and run correctly

--- Story.java
+++ Story.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
-
 public class Story{
 
     Scanner stringReader = new Scanner(System.in);
     Scanner numberReader = new Scanner(System.in);
 
-    public class Story(){
+    String fileSave;
+    String Intro;
+
+    public Story(){
+	setIntro();
 	getIntro();
     }
-    String Intro;
-    String fileSave;
     public void setIntro(){
-	System.out.print("Enter the introduction to your story");
-	Intro = stringReader.nextLine();//set intro that what the user typed in
+	System.out.println("Enter the introduction to your story");
+	Intro= stringReader.nextLine();//set intro that what the user typed in
     }
-	
-    public void  getIntro(){
+    public void getIntro(){
 	System.out.print(Intro);
     }
     public void saveStory(String fileName){}
     public String getFileSave(){
 	return fileSave;
     }
-    public void main(String args[]){}
+    public static void main(String[] args){
+	new Story();
+    }
 }
+
