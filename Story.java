9fa9e82c6693380dd35e603719e9374eadbc32ae
bchen70 <bchen17@stuy.edu>
2018-01-18 19:09:37 -0500

    Fixed getSections() to return the right order

--- Story.java
+++ Story.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 import java.util.*;
 public class Story{
 
-    Scanner stringReader = new Scanner(System.in);
+  Scanner stringReader = new Scanner(System.in);
 
-    String fileSave;
-    String Intro;
-    String ending;
-    String one,two;
-    String run;
-    String run2;
-    String ans1,ans2;
-    String sec1, sec2, sec3, sec4, sec5, sec6, sec7, sec8, sec9, sec10, sec11, sec12, sec13, sec14;
+  String fileSave;
+  String Intro;
+  String ending;
+  String one,two;
+  String run;
+  String run2;
+  String ans1,ans2;
+  String sec1, sec2, sec3, sec4, sec5, sec6, sec7, sec8, sec9, sec10, sec11, sec12, sec13, sec14;
 
   public Story(){
     setIntro();
@@ -50,15 +50,26 @@ public class Story{
     String askyesno = "";
     System.out.println("\nDo you want to play your story?Type 'yes' to play, type 'no' if you don't want to play\n");
     askyesno = stringReader.nextLine();
-    if (askyesno.equals("yes")){
+    if (askyesno.equals("yes\n")){
       System.out.println(Intro);
       System.out.println(selectOptions(0));
-      System.out.println(selectOptions(1));
-      System.out.println(selectOptions(2));
-      System.out.println(selectOptions(3));
-      System.out.println(selectOptions(4));
-      System.out.println(selectOptions(5));
-      System.out.println(selectOptions(6));
+      if (run.equals("one")){
+        System.out.println(selectOptions(1));
+        if (run.equals("one")){
+          System.out.println(selectOptions(3));
+        }
+        else{
+          System.out.println(selectOptions(4));
+        }  
+      }else{
+        System.out.println(selectOptions(2));
+        if (run.equals("one")){
+          System.out.println(selectOptions(5));
+        }
+        else{
+          System.out.println(selectOptions(6));
+        }
+      }
     }
     else {
       System.exit(0);
@@ -76,59 +87,62 @@ public class Story{
     ArrayList<String> s1 = new ArrayList<>();
     if (section == 0){
 	    if (run.equalsIgnoreCase("one")){
-        return sec1;
+        return "Section 1: "+  sec1;
       }
 	    else if(run.equalsIgnoreCase("two")){
-        return sec2;
+        return "Section 2: " +sec2;
       }
     }
     if (section == 1){
 	    if (run.equalsIgnoreCase("one")){
-        return sec3;
+        return "Section 3: "+ sec3;
       }
 	    else if(run.equalsIgnoreCase("two")){
-        return sec4;
+        return "Section 4: " +sec4;
       }
     }
     if (section == 2){
 	    if (run.equalsIgnoreCase("one")){
-        return sec5;
+        return "Section 5: "+ sec5;
       }
 	    else if(run.equalsIgnoreCase("two")){
-        return sec6;
+        return "Section 6: " +sec6;
 	    }
     }
     if (section == 3){
 	    if (run.equalsIgnoreCase("one")){
-        return sec7;
+        return "Section 7: "+ sec7;
 	    }
 	    else if(run.equalsIgnoreCase("two")){
-        return sec8;
+        return "Section 8: "+ sec8;
       }
     }
     if (section == 4){
 	    if (run.equalsIgnoreCase("one")){
-        return sec9;
+        return "Section 9: "+sec9;
       }
 	    else if(run.equalsIgnoreCase("two")){
-        return sec10;
+        return "Section 10: "+sec10;
       }
     }
     if (section == 5){
 	    if (run.equalsIgnoreCase("one")){
-        return sec11;
+        return "Section 11: "+sec11;
 	    }
 	    else if(run.equalsIgnoreCase("two")){
-        return sec12;
+        return "Section 12: "+sec12;
       }
     }
     if (section == 6){
 	    if (run.equalsIgnoreCase("one")){
-        return sec13;
+        return "Section 13: "+ sec13;
       }
 	    else if(run.equalsIgnoreCase("two")){
-        return sec14;
+        return "Section 14: "+ sec14;
 	    }
+    }
+    else {
+      throw new  IllegalArgumentException("Please type one or two!");
     }
     return "Please type a valid option!";
   }
@@ -136,49 +150,49 @@ public class Story{
   public String setOptions(int sect){
     String ans = "";
     if (sect == 0){
-	ans = "Intro";
+      ans = "Intro";
     }
     if (sect == 1){
-	ans = "Section 1";
+      ans = "Section 1 ";
     }
     if (sect == 2){
-	ans = "Section 2";
+      ans = "Section 2 ";
     }
     if (sect == 3){
-	ans = "Section 3";
+      ans = "Section 3 ";
     }
     if (sect == 4){
-	ans = "Section 4";
+      ans = "Section 4 ";
     }
     if (sect == 5){
-	ans = "Section 5";
+      ans = "Section 5 ";
     }
     if (sect == 6){
-	ans = "Section 6";
+      ans = "Section 6 ";
     }
     if (sect == 7){
-	ans = "Section 7";
+      ans = "Section 7 ";
     }
     if (sect == 8){
-	ans = "Section 8";
+      ans = "Section 8 ";
     }
     if (sect == 9){
-	ans = "Section 9";
+      ans = "Section 9 ";
     }
     if (sect == 10){
-	ans = "Section 10";
+      ans = "Section 10 ";
     }
     if (sect == 11){
-	ans = "Section 11";
+      ans = "Section 11 ";
     }
     if (sect == 12){
-	ans = "Section 12";
+      ans = "Section 12 ";
     }
     if (sect == 13){
-	ans = "Section 13";
+      ans = "Section 13 ";
     }
     if (sect == 14){
-	ans = "Section 14";
+      ans = "Section 14 ";
     }
     if (sect > 0 && sect < 8){
       System.out.println("\nPlease enter in option 1");
@@ -190,7 +204,7 @@ public class Story{
       System.out.println("\n" + ans + "Options:");
       getOptions();
     }
-    if (sect >=8 && sect < 15){
+    if (sect >=7 && sect < 15){
       System.out.println("\nPlease enter in option 1");
       one = stringReader.nextLine();
       System.out.println(">Option 1:" + one);
@@ -236,10 +250,6 @@ public class Story{
   public String getFileSave(){
     return fileSave;
   }
-  //  public void setNumOfOptions(){
-  //	System.out.println("Enter the number of options you want for this section");
-  //	numOfOptions = numberReader.nextInt();
-  // }
   public void getOptions(){
     System.out.println(">>>>Option 1:" + this.one);
     System.out.println(">>>>Option 2:" + this.two);
@@ -304,7 +314,7 @@ public class Story{
   public void Section13(){
     System.out.println("\nSection 13:Enter the story for this section based on option 1 of Section 6:");
     sec13 = stringReader.nextLine();
-      }
+  }
   public void Section14(){
     System.out.println("\nSection 14:Enter the story for this section based on option 2 of Section 6:");
     sec14 = stringReader.nextLine();
